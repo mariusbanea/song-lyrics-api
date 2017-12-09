08c87cbe8c3271d33c60c3d8f6cc2cc71c8a2751
Marius Banea <marius@w-d-s.co.uk>
2017-12-09 23:01:53 +0000

    updating the api structure

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-/* This app doesn't follow a11y best practices, and the JS file is incomplete. Complete the getDataFromApi, displaySearchData, and watchSubmit functions. When you're done, this app should allow a user to search for an artist and song title (both should be required), and display the song lyrics. You should make requests to this API: https://lyricsovh.docs.apiary.io/# . Also make any necessary adjustments to make this app accessible. */
+/* This app doesn't follow a11y best practices, and the JS file is incomplete.
+
+Complete the getDataFromApi, displaySearchData, and watchSubmit functions.
+
+When you're done, this app should allow a user to search for an artist and song title (both should be required), and display the song lyrics.
+
+You should make requests to this API: https://lyricsovh.docs.apiary.io/# .
+
+Also make any necessary adjustments to make this app accessible. */
+
+
+//how to build an API app
+
+
+// Step 1 - watch for user input; tell shopkeeper what shoe size,color
+// Step 1a - create a trigger
+// Step 1b - get user input
+// Step 1c - use the api function
+
+
+// Step 2 - define the function to make the api call; shopkeeper goes to warehouse to get shoe
+// Step 2a - make the api call using the URL, dataType (JSON or JSONP), type (GET or POST)
+// Step 2b - success scenario (call the function to display the results)
+// Step 2c - failure scenario (display errors)
+
+
+// Step 3 - display the results; sales process
+// Step 3a - console.log the results
+// Step 3b - create an HTML results variable
+// Step 3c - use a for loop to populate the empty HTML results variable (.each)
+// Step 3d - send the content of HTML results variable to the HTML ($("class or id name from the HTML").html(HTML results variable))
 
 function getDataFromApi(artist, title) {
+
+    //full AJAX intro https://www.w3schools.com/xml/ajax_intro.asp
 
     // make the api call
     $.ajax({
